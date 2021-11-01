e4bf1b42254858322af4be5d982102cdf555833c
Shubh9907 <shubhamv60047@gmail.com>
2021-11-01 22:29:18 +0530

    Welcome Message Added

---
+++ src/com/bridgelabz/LinkList.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+public class LinkList {
+    Node head;
+    Node tail;
+
+    public Node push(int data){
+        Node newNode = new Node(data);
+        if (head==null){
+            this.head=newNode;
+            this.tail=newNode;
+        }else {
+            newNode.next = head;
+            this.head = newNode;
+        }
+        return newNode;
+    }
+    public void show(){
+        Node temp = head;
+        if (head==null){
+            System.out.println("Linked List is Empty");
+        }else {
+            while (temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+}
